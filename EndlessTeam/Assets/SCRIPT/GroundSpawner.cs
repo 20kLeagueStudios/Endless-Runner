98d0000fe5240943ee1d596e93a339ceb6085a77
emanuelefazio <emanuelefaziovfx@gmail.com>
2021-06-09 18:40:42 +0200

    Spawn_blocchi(Emanuele)
    spawn dei blocchi prefab, setup base

---
+++ EndlessTeam/Assets/SCRIPT/GroundSpawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundSpawner : MonoBehaviour
+{
+
+    public GameObject groundTile;
+    Vector3 nextSpawnPoint;
+
+   public void SpawnTile()
+    {
+       GameObject go = Instantiate(groundTile, nextSpawnPoint, Quaternion.identity);
+
+        nextSpawnPoint = go.transform.GetChild(1).transform.position; // nel progetto il gameobject nextSpawnPoint è il secondo figlio del prefab groundtile, quindi a indice 1
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 10 ; i++)
+        {
+            SpawnTile();
+        }
+
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
