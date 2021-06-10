dbd832b47b66a03fe8c6a9a628d951f4cc4fdabf
Pytodoom <kevincicero98@gmail.com>
2021-06-10 21:37:24 +0200

    Finalizzazione movimento

--- EndlessTeam/Assets/Scripts/PlayerMovement.cs
+++ EndlessTeam/Assets/Scripts/PlayerMovement.cs
@@ -31,7 +31,7 @@ public class PlayerMovement : MonoBehaviour
     /// </summary>
     bool isTouching, canSwipe;
 
-    public bool sliding = false;
+    bool sliding = false;
 
     //Enum di Swipe per ogni carreggiata
     enum Swipe
@@ -44,37 +44,43 @@ public class PlayerMovement : MonoBehaviour
     //Inizializziazione dell'enum Swipe
     Swipe swipeEn;
 
-    ///COMMENTI DA INSERIRE
+    //Gravità, forza di salto e vettore in cui verrà applicato all'asse Y la gravità e il salto
     float gravity = -30.81f;
     float jumpForce = 6;
-    Vector3 gravityForce = default;
+    Vector3 verticalForce = default;
 
+    //Posizione da cui creare una sfera invisibile che controllerà se si tocca il pavimento o no
     [SerializeField]
     Transform groundCheck;
 
+    //Layer del pavimento
     [SerializeField]
     LayerMask groundMask;
 
+    //Altezza che dovrà avere il character controller e la posizione che dovrà avere il punto Y del character controller
+    //Quando si utilizzerà lo slide
     [SerializeField]
     float crouchHeight, crouchPos;
 
+    //Stessa cosa dello slide ma per ritornare allo stato di corsa
     float idleHeight, idlePos;
 
+    //Riferimento all'animatore
     [SerializeField]
     Animator animator;
 
     private void Start()
     {
+        //Setto l'altezza standard a quella iniziale
         idleHeight = controller.height;
         idlePos = controller.center.y;
+
         //Assegno di default il valore mid all'enum swipe
         swipeEn = Swipe.Mid;
     }
 
     void Update()
     {
-        bool isGround = Physics.CheckSphere(groundCheck.position, .4f, groundMask);
-
         //Se si sta toccando lo schermo
         if (Input.touchCount > 0)
         {
@@ -137,22 +143,31 @@ public class PlayerMovement : MonoBehaviour
             //Se posso effettuare lo swipe
             if (canSwipe)
             {
-                Debug.Log(isGround);
+                //Controllo se sto toccando il pavimento
+                bool isGround = Physics.CheckSphere(groundCheck.position, .4f, groundMask);
+                //Se faccio lo swipe in alto
                 if (swipeEn == Swipe.Up)
                 {
+                    //Se tocco il pavimento
                     if (isGround)
                     {
-                        gravityForce.y = Mathf.Sqrt(jumpForce * -2f * gravity);
-                        if (sliding) ResetSliding();
-                      
+                        //Applico la forza del salto a verticalForce
+                        verticalForce.y = Mathf.Sqrt(jumpForce * -2f * gravity);
+                        //Se sto usando lo sliding lo disattivo chiamando ResetSliding()
+                        if (sliding) ResetSliding();              
                     }
-                    
                 }
+                //Altrimenti se sto effettuando lo swipe in basso
                 else if (swipeEn == Swipe.Down)
                 {
+                    //Se sto toccando il pavimento e non sto già usando lo slide attivo la coroutine SlideCor
                     if (isGround && !sliding)
                         StartCoroutine("SlideCor");
+                    //Altrimenti applico una forza al verticalForce che spingerà più velocemente in basso il giocatore
+                    else if (!isGround)
+                        verticalForce.y = -34f;
                 }
+                //Altrimenti se sto facendo uno swipe orizzontale
                 else
                 {
                     //Fermo la coroutine nel caso in cui dovesse essere ancora attiva per la chiamata precedente
@@ -161,28 +176,32 @@ public class PlayerMovement : MonoBehaviour
                     StartCoroutine(ChangingPosition(swipeEn.ToString()));
                     //Non posso effettuare lo swipe fino all'inizio del prossimo tocco
                 }
+                //rendo canSwipe a false, in questo modo per fare un nuovo swipe dovrò ripoggiare il dito
                 canSwipe = false;
             }
 
            
         }
 
-        ///COMMENTI DA INSERIRE
-        gravityForce.y += gravity * Time.deltaTime;
+        //Aggiungo la gravità alla verticalForce
+        verticalForce.y += gravity * Time.deltaTime;
 
-        controller.Move(gravityForce * Time.deltaTime);
+        //Applico verticalForce al controller
+        controller.Move(verticalForce * Time.deltaTime);
 
+        //Se sto toccando il pavimento e la forza è minore di zero verrà messa di default a -3 così che
+        //non raggiunga valori immensi
         if (Physics.CheckSphere(groundCheck.position, .2f, groundMask))
         {
-            if (gravityForce.y < 0) gravityForce.y = -3f;
-
+            if (verticalForce.y < 0) verticalForce.y = -3f;
         }
 
         //Movimento continuo in avanti
         controller.Move(transform.forward * movSpeed * Time.deltaTime);
     }
 
-    private void ResetSliding()
+    //Disattivo lo sliding, e setto a false l'animazione dello slide così che esca e ritorni allo stato corsa
+    public void ResetSliding()
     {
         sliding = false;
         animator.SetBool("Slide", false);
@@ -261,28 +280,39 @@ public class PlayerMovement : MonoBehaviour
         }
 
     }
+
+    //Si occupa di effettuare lo slide
     public IEnumerator SlideCor()
     {
+        //Rendo lo sliding a true così che SlideCor() non può essere chiamato mentre è in esecuzione
         sliding = true;
+        //Attivo l'animazione dello slide
         animator.SetBool("Slide", true);
+        //Setto la posizione dei vettori che determineranno l'altezza e la posizione in slide del controller
         Vector3 controllerCrouchPos = new Vector3(0,crouchPos,0);
         Vector3 controllerIdlePos = new Vector3(0, idlePos, 0);
+        //Se l'altezza corrente del controller è diversa da quella in slide, significa che dobbiamo assegnarla
         if (controller.height != crouchHeight)
         {
+            //Setto l'altezza e la posizione in slide
             controller.height = crouchHeight;
             controller.center = controllerCrouchPos;
             yield return null;
         }
+        //Finché slide è vero rimane bloccata la coroutine, sliding verrà messo a true quando faremo lo swipe in alto
+        //o quando l'animazione starà quasi per finire
         while (sliding)
         {
             yield return null;
         }
+        //Dopo di che l'altezza e la posizione del controller viene messa in modalità corsa
         controller.height = idleHeight;
         controller.center = controllerIdlePos;
 
         yield return null;
     }
 
+    //Debug del groundCheck
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
--- EndlessTeam/Assets/SlideSTM.cs
+++ EndlessTeam/Assets/SlideSTM.cs
@@ -5,27 +5,36 @@ using UnityEngine;
 public class SlideSTM : StateMachineBehaviour
 {
 
+    /// <summary>
+    /// Questo stm si occupa di rendere a true lo sliding del PlayerMovement quando l'animazione starà per finire
+    /// in questo modo verrà gestito tutto correttamente all'interno dello script PlayerMovement
+    /// </summary>
+
+
+    //Riferimento allo script playerMovement da cui prenderemo la booleana sliding per settarla a false
     PlayerMovement playerMovement;
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        //Prendo il riferimento playerMovement dal parent
         playerMovement = animator.GetComponentInParent<PlayerMovement>();
     }
 
-    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        //Se l'animazione è finita
         if (animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 1 && !animator.IsInTransition(0))
         {
-            animator.SetBool("Slide", false);
-            playerMovement.sliding = false;
+            //Chiamo il metodo ResetSliding del playerMovement
+            playerMovement.ResetSliding();
         }
     }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        playerMovement.sliding = false;
-        
+        //playerMovement.sliding = false;
+        //playerMovement.ResetSliding();
     }
 
     // OnStateMove is called right after Animator.OnAnimatorMove()
