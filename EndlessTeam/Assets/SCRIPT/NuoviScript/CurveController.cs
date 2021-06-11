56e2dc62861f18df0f819376caf369197450a8ac
emanuelefazio <emanuelefaziovfx@gmail.com>
2021-06-11 17:02:46 +0200

    Seconda prova spawn blocchi + shader curvatura
    1 - Una seconda versione di spawn dei blocchi (sono i blocchi ad avanzare verso il player che rimane fermo sul posto)
    
    2 - shader curvatura (seconda prova)
    più semplice da gestire, tre variabili esposte nell inspector permettono di cambiare a runtime la curvatura sia su y che x. è possibile anche cambiare a runtime il punto di origine della distorsione

---
+++ EndlessTeam/Assets/SCRIPT/NuoviScript/CurveController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CurveController : MonoBehaviour
+{
+
+    public Transform CurveOrigin; //nelnostro caso il punto di origine è la camera
+
+    [Range(-500f, 500f)]
+    [SerializeField]
+    float x = 0f;
+
+    [Range(-500f, 500f)]
+    [SerializeField]
+    float y = 0f;
+
+    [Range(0f, 50f)]
+    [SerializeField]
+    float falloff = 0f;
+
+    private Vector2 bendAmount = Vector2.zero;
+
+    // Global shader property ids
+    private int bendAmountId;
+    private int bendOriginId;
+    private int bendFalloffId;
+
+    void Start()
+    {
+        bendAmountId = Shader.PropertyToID("_BendAmount"); //i tre int sono inizializzati al valore dei label presenti nello script dello shader
+        bendOriginId = Shader.PropertyToID("_BendOrigin");
+        bendFalloffId = Shader.PropertyToID("_BendFalloff");
+    }
+
+    void Update()
+    {
+        bendAmount.x = x;
+        bendAmount.y = y;
+
+        Shader.SetGlobalVector(bendAmountId, bendAmount);
+        Shader.SetGlobalVector(bendOriginId, CurveOrigin.position);
+        Shader.SetGlobalFloat(bendFalloffId, falloff);
+    }
+
+}
