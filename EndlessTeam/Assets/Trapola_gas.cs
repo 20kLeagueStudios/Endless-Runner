8839ef52a9a562fc2039316dc60c92bf33f2220a
Fiamma <77980204+gFiamma@users.noreply.github.com>
2021-06-25 19:21:19 +0200

    Trappole Corda e Gas
    una trappola dove cade un "tronco" l'altro dove innesca una "esplosione"

---
+++ EndlessTeam/Assets/Trapola_gas.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trapola_gas : MonoBehaviour
+{
+    public GameObject inesco;
+
+    //public GameObject tronco;
+    [SerializeField]
+    private Animator aim;
+    [SerializeField]
+    private Animator cc;
+    // Start is called before the first frame update
+    void Start()
+    {
+        inesco.SetActive(true);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            V_Inesco();
+            Debug.Log("OK");
+        }
+
+    }
+    void V_Inesco()
+    {
+        inesco.SetActive(false);
+            cc.SetTrigger("Trigger_Inesco");
+        aim.SetTrigger("Trigger_Gas");
+    }
+
+}
