008705e7cd7c6355a52c68bc69a2db6b7a4991a3
Pytodoom <kevincicero98@gmail.com>
2021-06-28 14:27:07 +0200

    Score

--- EndlessTeam/Assets/Scripts/Player/PlayerMovement.cs
+++ EndlessTeam/Assets/Scripts/Player/PlayerMovement.cs
@@ -19,6 +19,11 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     Transform[] positions;
 
+    Vector3 obstaclePos;
+
+    [SerializeField]
+    Score scoreScript;
+
     /// <summary>
     /// <var name="startPos"> Posizione del tocco iniziale.</var>
     /// <var name="swipeDelta"> Differenza di posizione tra tocco iniziale e posizione corrente del tocco.</var>
@@ -69,6 +74,8 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     Animator animator;
 
+    GameObject currentObstacle;
+
     private void Start()
     {
         //Setto l'altezza standard a quella iniziale
@@ -81,6 +88,7 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
+
         //Se si sta toccando lo schermo
         if (Input.touchCount > 0)
         {
@@ -201,6 +209,7 @@ public class PlayerMovement : MonoBehaviour
         controller.Move(transform.forward * movSpeed * Time.deltaTime);
     }
 
+
     //Disattivo lo sliding, e setto a false l'animazione dello slide così che esca e ritorni allo stato corsa
     public void ResetSliding()
     {
@@ -266,13 +275,13 @@ public class PlayerMovement : MonoBehaviour
         {
      
             //Lerpo la posizione a quella finale
-            dest.x = Mathf.Lerp(dest.x, finalPos.x, .1f);
+            dest.x = Mathf.Lerp(dest.x, finalPos.x, .2f);
             dest.y = transform.position.y;
             dest.z = transform.position.z;
 
             //Se manca poco all'arrivo della posizione, viene direttamente messa uguale alla posizione finale
             //In questo modo si evitano loop infiniti
-            if (Mathf.Abs(dest.x - finalPos.x) < .6f)
+            if (Mathf.Abs(dest.x - finalPos.x) < .4f)
                 dest.x = finalPos.x;
 
             //Assegno continuamente la posizione lerpata a quella effettiva del player
@@ -313,6 +322,18 @@ public class PlayerMovement : MonoBehaviour
         controller.center = controllerIdlePos;
 
         yield return null;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.CompareTag("Obstacle"))
+        {
+            currentObstacle = other.gameObject;
+        } else if (other.CompareTag("Point") && other.transform.parent.gameObject != currentObstacle)
+        {
+            scoreScript.TakeScore(3);
+        }
     }
 
     //Debug del groundCheck
