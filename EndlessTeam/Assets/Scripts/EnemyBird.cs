feb645279296ca6216c64877617944e99bc6a40b
Francesco-Platania <francesco.platania1998@gmail.com>
2021-06-30 11:46:43 +0200

    aggiunta scena e script di emanuele

---
+++ EndlessTeam/Assets/Scripts/EnemyBird.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBird : MonoBehaviour
+{
+
+    public float verticalSpeed;
+    public float amplitude;
+
+    Vector3 tmpPos;
+
+    Vector3 startPos;
+
+    private void Awake()
+    {
+        tmpPos = transform.localPosition;
+        startPos = transform.localPosition;
+
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tmpPos = transform.localPosition;
+        startPos = transform.localPosition;
+
+    }
+
+    private void OnEnable()
+    {
+        transform.localPosition = startPos;
+        StartCoroutine(FlyPingPong());
+    }
+
+    IEnumerator FlyPingPong()
+    {
+        float waitTime=10f;
+        float elapsedTime = 0f;
+
+        float pingpong;
+
+        while (elapsedTime<waitTime)
+        {
+            elapsedTime += Time.deltaTime;
+
+            pingpong = Mathf.PingPong(Time.time * 4, -1-1) + 2 ;
+
+            tmpPos.x = pingpong;
+
+            tmpPos.y = Mathf.Sin((Time.time * verticalSpeed) * amplitude) + 2;
+
+            //transform.localPosition =  Vector3.Lerp(startPos,tmpPos,(elapsedTime/waitTime)*4);
+
+            transform.localPosition = new Vector3(Mathf.PingPong((elapsedTime / waitTime) * 12, 6) - 3 , tmpPos.y, transform.localPosition.z);
+
+
+            yield return null;
+        }
+
+        yield return null;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ EndlessTeam/Assets/Scripts/PortalScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PortalScript : MonoBehaviour
+{
+    int i = 0;
+    void Start()
+    {
+        
+    }
+
+  /*
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            SceneManager.UnloadSceneAsync("Scene01");
+
+            Debug.Log("SCARICA");
+
+        }
+    }
+    
+
+   
+    private void OnEnable()
+    {
+        if (i == 0)
+        {
+            SceneManager.LoadScene(1, LoadSceneMode.Additive);
+            i++;
+        }
+        
+
+    }
+   */
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
