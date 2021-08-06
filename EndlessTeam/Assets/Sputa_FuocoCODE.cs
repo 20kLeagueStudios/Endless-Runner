fb5e5373c8b48d79a9f773d835294c6a8af08749
Francesco <francesco.platania1998@gmail.com>
2021-08-06 18:44:52 +0200

    Totem sputa fuoco
    manca da sistemare il timing tra bocca e fuoco

---
+++ EndlessTeam/Assets/Sputa_FuocoCODE.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sputa_FuocoCODE : MonoBehaviour
+{
+    [SerializeField]
+    private Animator aim;
+    [SerializeField]
+   ParticleSystem TotFuoco = null;
+
+    // Update is called once per frame
+     void Start()
+    {
+        TotFuoco.Stop();
+    }
+    void OnTriggerEnter(Collider c)
+    {
+
+        if (c.gameObject.CompareTag("Player"))
+        {
+            aim.SetTrigger("Trigger_Fuoco");
+            TotFuoco.Play();
+        }
+
+    }
+}
