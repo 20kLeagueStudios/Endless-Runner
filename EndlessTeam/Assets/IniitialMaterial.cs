e24448a57213b953af8c32afb1fa755b1911e660
Pytodoom <kevincicero98@gmail.com>
2021-08-30 18:05:45 +0200

    Fix

--- EndlessTeam/Assets/IniitialMaterial.cs
+++ EndlessTeam/Assets/IniitialMaterial.cs
@@ -13,11 +13,32 @@ public class IniitialMaterial : MonoBehaviour
     void Material()
     {
         Renderer[] temp = transform.GetComponentsInChildren<Renderer>();
-        Debug.Log("SEEEEE" + temp.Length);
         foreach (Renderer rend in temp)
         {
             rend.enabled = false;
 
+        }
+
+        Invoke("MaterialFix",1);
+    }
+
+    void MaterialFix()
+    {
+        Renderer[] rendererFirst = transform.GetComponentsInChildren<Renderer>();
+
+        for (int i = 0; i < rendererFirst.Length; i++)
+        {
+            Renderer temp = rendererFirst[i];
+
+            bool tags = temp.CompareTag("Money") || temp.CompareTag("Enemy") || temp.CompareTag("Portal") || temp.CompareTag("PortalShader") || temp.CompareTag("PowerUp");
+            if (!tags)
+            {
+                if (!temp.CompareTag("Invisible")) temp.enabled = true;
+            }
+            else if (temp.CompareTag("Money")) {  temp.enabled = true; }
+            else if (temp.CompareTag("Enemy")) { temp.enabled = true; }
+            else if (temp.CompareTag("Portal")) { temp.enabled = true; }
+            else if (temp.CompareTag("PowerUp")) { temp.enabled = true; }
         }
     }
 }
